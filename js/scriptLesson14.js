0be6c701a8091dae589d43e88902cf3b9e7fe086
Konstantin <kuzprodev.com>
2022-03-18 08:12:41 +0200

    +fileJs

---
+++ js/scriptLesson14.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let num = 50;
+// while(num<=55){
+//     console.log(num);
+//     num++;
+// }
+do{//сделай пожалуйста что-то 
+    console.log(num);
+    num++;
+}
+while(num<55)
+
+for(let i =1;i<8;i++){
+console.log(num);
+num++;
+}
+for(let i =1;i<10;i++){
+    if(i===6){
+// break;
+continue;//позволяет пропустить тот шаг который нам не нужен но при этом не останавливает полностью цикл
+    }
+console.log(i);
+
+}
+//lesson14 циклы
