d246af34d171e31fe26ce3aa6de6615d529f5e72
Konstantin <kuzprodev.com>
2022-03-24 11:32:45 +0200

    lesson19_callback

--- js/script.js
+++ js/script.js
@@ -1,2 +1,26 @@
 "use strict"
 // lesson19 callback func
+
+function first() {
+    //Do something
+    setTimeout(function () {
+        console.log(1);
+    }, 500)
+}
+
+function second() {
+    console.log(2);
+}
+first();
+second();
+// callback-функция которая должна будет выполнится после того как другая завершила своё выполнение
+function learnJS(lang, callback) {
+    console.log(`Я учу:${lang}`);
+    callback();
+}
+
+function done() {
+
+    console.log("Я прошел этот урок!");
+};
+learnJS('JavaScript', done);
