69522964d7701ba1c9ffffe6a2c10ad95622a775
Konstantin <kuzprodev.com>
2022-03-26 12:42:28 +0200

    lesson20obj

--- js/script.js
+++ js/script.js
@@ -1,25 +1,57 @@
 "use strict"
-// lesson19 callback func
-
-function first() {
-    //Do something
-    setTimeout(function () {
-        console.log(1);
-    }, 500)
-}
-
-function second() {
-    console.log(2);
-}
-first();
-second();
-// callback-функция которая должна будет выполнится после того как другая завершила своё выполнение
-function learnJS(lang, callback) {
-    console.log(`Я учу:${lang}`);
-    callback();
-}
-
-function done() {
-    console.log("Я прошел этот урок!");
+// lesson20 Объекты, деструктуризация obj
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function () {
+        console.log("Test");
+    }
 };
-learnJS('JavaScript', done);
+options.makeTest();
+
+// деструктуризация объекта:
+const {
+    border,
+    bg
+} = options.colors;
+console.log(border, bg);
+
+console.log(Object.keys(options).length);
+console.log(options.colors.bg);
+console.log(options['colors']['border']);
+
+delete options.name; //удалить
+
+for (let key in options) { //перебираем обьект key-просто переменная и этот цикл будет работать столько раз сколько свойств в объекте
+    if (typeof (options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+        }
+    } else {
+
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+    }
+}
+// у объекта нет св-ва length но узнать кол-во можно:
+// console.log(Object.keys(options).length);
+let counter = 0;
+for (let key in options) { //перебираем обьект key-просто переменная и этот цикл будет работать столько раз сколько свойств в объекте
+    // if (typeof (options[key]) === 'object') {
+    //     for (let i in options[key]) {
+    //         console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+    counter++;
+    //     }
+    // } else {
+
+    //     console.log(`Свойство ${key} имеет значение ${options[key]}`);
+    //     counter++;
+    // }
+}
+console.log(counter);
+
+// JS это объектно-ориентированный язык и все сущности которые в нём есть сводятся к объектам=прототипно-ориентированный язык
