b6ccb98a7f648e4ca79485e3235f8ae15e31f2a7
Konstantin <kuzprodev.com>
2022-03-28 10:49:15 +0300

    lesson22 Передача по ссылке или значению, Spread оператор(ES6-ES9)

--- js/script.js
+++ js/script.js
@@ -1,33 +1,94 @@
 "use strict"
-// lesson21 Массивы и псевдомассивы
-const arr = [1, 2, 5, 7, 9];
-arr.sort(compareNum); //алгоритм sort внутри себя использует алгоритм быстрой сортировки
-console.log(arr);
+// lesson22 Передача по ссылке или значению, Spread оператор(ES6-ES9)
+let a = 5;
+b = a;
+b = d + 5;
+console.log(b, a);
+// const obj = {
+//     a: 5,
+//     b: 1
+// }
 
-function compareNum(a, b) {
-    return a - b;
+// const copy = obj;
+// copy.a = 10;
+// console.log(copy);
+// console.log(obj);
+//  в объектах если меняешь копию, то и оригин меняется
+//  тоесть когда мы работаем с примитивными типами данных они передаются по значению
+// а когда мы работаем с обьектами(массивы функции и типа обьекты даты) идет передача по ссылке
+
+function copy(mainObj) {
+    let objCopy = {};
+
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+        return objCopy;
+    }
 }
 
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
+const newNumbers = copy(numbers);
+newNumbers.a = 10;
+newNumbers.a.x = 10;
+console.log(newNumbers);
+console.log(numbers);
 
-// arr[99]=0;
-// console.log(arr.length);
-// console.log(arr);
-arr.forEach(function (item, index, arr) {
-    console.log(`${index}: ${item} внутри массива ${arr}`);
 
-}); //метод перебора массивов
-arr.pop(); //удаляет посл элемент из массива
-arr.push(10); //добавляет посл элемент из массива
-console.log(arr);
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
+const add = {
+    d: 17,
+    e: 20
+};
+
+// console.log(Object.assign(numbers, add));
+const clone = Object.assign({}, add); //так можно тоже создавать копии обьектов
+clone.d = 21;
+console.log(add);
+console.log(clone);
+
+
+// создатькопию массива:
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+
+newArray[1] = 'addss';
+console.log(newArray);
+console.log(oldArray);
+
+// оператор разворота spread - разворачивает структуру и превращает в набор каких-то данных
+const video = ['youtube', 'vumeo', 'rutube'],
+    blogs = ['wordpress', 'livejornal', 'blogger'],
+    internet = [...video, ...blogs, 'vk', 'facebook']; //просто вытаскиваем элементы и они вставляются в переменную internet через запяту
+console.log(internet);
+
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
 }
 
-for (let value of arr) {
-    console.log(value);
-}
-const str = prompt("", "");
-const products = str.split(", "); // вывод массива без ,_
-// console.log(products);
-products.sort(); // метод сортировки всегда как строки
-console.log(products.join("; ")); //метод обьеденения элементов в массиве
+const num = [1, 2, 3];
+
+log(...num);
+
+
+
+const array = ['a', 'b'];
+const newAarray = [...array];
+
+const q = {
+    one: 1,
+    two: 2
+};
+
+const newObj = {
+    ...q
+};
