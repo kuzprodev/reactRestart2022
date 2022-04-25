fc9135137e30fad4c88dadff3832456bf0f7e9f8
Konstantin <kuzprodev.com>
2022-04-25 17:28:47 +0300

    addLesson26_24 аргументы функции

--- js/addLesson.js
+++ js/addLesson.js
@@ -1,46 +1,10 @@
-// 21 логические операторы
-// const hamburger = true;// 5
-// const fries = true;//null, 0
-// if(hamburger && fries){
-//     console.log("Я сыт!");
-// }
-// console.log((hamburger && fries));
-//результат логических операций это всегда какоето булевое значение true или false
+// 24 аргументы функции
 
+const usdCurr = 28;
+const eurCurr = 32;
 
-
-// const hamburger = 3; 
-// const fries = 1;
-// const cola =0;
-console.log((hamburger === 3 && cola && fries)); //оператор && возвращает первое значение на котором он остановился, а если все правильно то возвратит тот на котором остановился
-
-if(hamburger === 3 && cola === 1 && fries === 1){
-    console.log("Все довольны!");
+ function convert(amount, curr){
+    console.log(curr * amount);
 }
-else{
-    console.log("Мы уходим!");
-
-}
-
-console.log(1 && 0);
-console.log(1 && 5);
-console.log(null && 5);
-console.log(0 && 'efefefesgwrf');
-
-
-
-const hamburger = 3; 
-const fries = 3;
-const cola = 0;
-const nuggets = 2;
-
-if(hamburger === 3 && cola === 2 || fries === 3 && nuggets){
-    console.log("Все сыты!");
-}
-else{
-    console.log("Мы уходим!");
-
-};
-
-let johnReport, alexReport, samReport, mariaReport = 'done';
-console.log(johnReport || alexReport || samReport || mariaReport);
+convert(500, usdCurr)
+convert(500, eurCurr)
