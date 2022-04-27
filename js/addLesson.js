b4db91557c6590cc9bebc63a8564082d8efdd565
Konstantin <kuzprodev.com>
2022-04-27 07:31:03 +0300

    addLesson_27 про важность return

--- js/addLesson.js
+++ js/addLesson.js
@@ -1,10 +1,39 @@
 // 24 аргументы функции
 
+// const usdCurr = 28;
+// const eurCurr = 32;
+
+//  function convert(amount, curr){
+//     console.log(curr * amount);
+// }
+// convert(500, usdCurr)
+// convert(500, eurCurr)
+
+//27 про важность return
+
 const usdCurr = 28;
-const eurCurr = 32;
+const discount = 0.9;
 
  function convert(amount, curr){
-    console.log(curr * amount);
+    return curr * amount;
 }
-convert(500, usdCurr)
-convert(500, eurCurr)
+function promotion(result){
+console.log(result * discount);
+}
+
+
+const res =convert(500, usdCurr);
+promotion(convert(500, usdCurr));
+
+
+function test (){
+    for(let i = 0; i < 5; i++){
+        console.log(i);
+        if(i === 3) return
+    }
+    console.log('Done');
+}
+test();
+
+function doNothing(){};
+console.log(doNothing() === undefined);
