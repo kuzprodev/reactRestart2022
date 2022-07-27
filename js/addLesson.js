8adf252eb85174a591ab15a3a626c25425a21e17
Konstantin <kuzprodev.com>
2022-07-27 09:42:56 +0300

    testworkgit

--- js/addLesson.js
+++ js/addLesson.js
@@ -1,39 +1,10 @@
-// 24 аргументы функции
-
-// const usdCurr = 28;
-// const eurCurr = 32;
-
-//  function convert(amount, curr){
-//     console.log(curr * amount);
-// }
-// convert(500, usdCurr)
-// convert(500, eurCurr)
-
-//27 про важность return
-
-const usdCurr = 28;
-const discount = 0.9;
-
- function convert(amount, curr){
-    return curr * amount;
-}
-function promotion(result){
-console.log(result * discount);
+// lesson40 замыкание и лексическое окружение
+'use strict';
+let number = 5;
+function logNumber(){
+    console.log(number);
 }
 
+number = 6;
 
-const res =convert(500, usdCurr);
-promotion(convert(500, usdCurr));
-
-
-function test (){
-    for(let i = 0; i < 5; i++){
-        console.log(i);
-        if(i === 3) return
-    }
-    console.log('Done');
-}
-test();
-
-function doNothing(){};
-console.log(doNothing() === undefined);
+logNumber();
