2644e7ce38bfd6783ae1d1cf0d78e362496f8dc8
Konstantin <kuzprodev.com>
2022-08-04 15:04:52 +0300

    lesson 45. События и их обработчики

--- js/script.js
+++ js/script.js
@@ -1,66 +1,52 @@
-// lesson 43. Действия с элементами на странице
+// lesson 45. События и их обработчики
 'use strict';
 
-const box = document.getElementById('box'),
-    btns = document.getElementsByTagName('button'),
-    circles = document.getElementsByClassName('circle'),
-    hearts = document.querySelectorAll('.heart'),
-    oneHeart = document.querySelectorAll('.heart'),
-    wrapper = document.querySelector('.wrapper');
-
-// console.dir(box);
-box.style.backgroundColor = 'blue';
-box.style.width = '500px';
-let num = 333
-box.style.cssText = `background-color: blue; width: ${num}px`;
+const btn = document.querySelector('button'),
+overlay = document.querySelector('.overlay'),
+btns =document.querySelectorAll('button');
 
 
-btns[1].style.borderRadius = '100%';
-circles[0].style.backgroundColor = 'red';
+// btn.onclick = function () { //але так вже не робиться застаріла версія- може спрацювати тільки
+//     // один раз-останній+неможливо видалити подію
+//     alert('Click');
+// };
+// btn.onclick = function () { //але так вже не робиться застаріла версія- може спрацювати тільки
+//     // один раз-останній+неможливо видалити подію
+//     alert('Second Click');
+// };
 
-// for(let i =0; i < hearts.length; i++){
-//     hearts[i].style.backgroundColor = 'blue';
-// }  // почти не будем использовать, так как есть спец переберающие методы
+// btn.addEventListener('click', () => {
+//     alert('new click');
+// });
+// btn.addEventListener('click', () => {
+//     alert('new second click');
+// });
+// btn.addEventListener('mouseenter', (e) => {
+//     console.log(e);
+//     console.log(e.target);
+//     // e.target.remove();
+//     console.log('hover');
+// });
+// let i = 0;
+const deleteElement = (e) => {
+    // e.target.remove();
+    console.log(e.target);
+    console.log(e.currentTarget);
+    console.log(e.type); //тип события
+    // i++;
+    // if (i === 1) {
+    //     btn.removeEventListener('click', deleteElement); //удаление обработчика
+    // }
+};
 
-hearts.forEach(item => {
-    item.style.backgroundColor = 'blue';
+// btn.addEventListener('click', deleteElement);
+overlay.addEventListener('click', deleteElement);
+
+const link =document.querySelector('a');
+link.addEventListener('click', (e)=>{
+   e.preventDefault(); //отмена стандартного поведения
+    console.log(e.target);
 });
-
-
-const div = document.createElement('div');
-// const text = document.createTextNode('Тут был Я')
-
-
-div.classList.add('black');
-
-// document.body.append(div);
-// document.querySelector('.wrapper').append(div);
-wrapper.append(div);  //вставляет в конец блока
-wrapper.prepend(div);  //вставляет в начало блока
-wrapper.appendChild(div);
-
-
-hearts[1].before(div);  // перед блоком
-hearts[1].after(div);   //после блока
-
-
-wrapper.insertBefore(div, hearts[0]);//старый метод  //первый аргумент-что вставляем, второй аргумент -перед каким элементом вставляем
-
-
-circles[0].remove();  // метод удаление элемента
-wrapper.removeChild(hearts[1]);//старый метод
-
-hearts[0].replaceWith(circles[0]);  //метод заменить один элемент другим
-// wrapper.replaceChild(circles[1], hearts[0]);  //старый метод 1-на который поменять,2-тот который меняется
-
-
-
-div.innerHTML='<h1>Hello World</h1>';
-
-// div.textContent = 'Hello'; //только с текстом
-
-
-div.insertAdjacentHTML('beforebegin', '<h2>Hello</h2>'); //перед элементом
-div.insertAdjacentHTML('afterbegin', '<h2>Hello</h2>'); // первым внутрь элемента
-div.insertAdjacentHTML('beforeend', '<h2>Hello</h2>'); // последним внутрь элемента
-div.insertAdjacentHTML('afterend', '<h2>Hello</h2>'); // после элемента
+btns.forEach(btn=>{
+    btn.addEventListener('click', deleteElement,{once:true} ); //у события есть третья опция(аргумент)
+});
