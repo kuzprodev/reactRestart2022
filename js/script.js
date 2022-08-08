79c6876ea37b246d37f4cc098122c677f9643c6f
Konstantin <kuzprodev.com>
2022-08-08 10:01:21 +0300

    lesson 53. (д) ператор нулевого слияния (Nullish, ??) ES11

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
-// lesson 50. Async, defer, динамические скрипты
+//lesson 53. (д) Оператор нулевого слияния (Nullish, ??) ES11
+
 'use strict';
 // document.addEventListener('DOMContentLoaded', () => {
 
+const box = document.querySelector('.box');
+const newHeight = 100;
+const newWidth = 400;
 
-const loadScript = (src)=>{
-    const script =document.createElement('script');
-    script.src = src;
-    script.async =false;
-    document.body.append(script);
+const changeParams = (elem, h, w) => {
+    elem.style.height = `${h ?? 200}px`;
+    elem.style.width = `${w ?? 200}px`;
+    elem.innerHTML = (h ?? 100) * (w ?? 300);
 };
-loadScript("js/test.js");
-loadScript("js/test2.js");
+changeParams(box, newHeight, newWidth);
+
+let userName;
+let userKey;
+console.log(userName ?? userKey ?? 'User');
+
 // });
