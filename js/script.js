2a8c23f5f6eee15604352072fa77e21d7c28754c
Konstantin <kuzprodev.com>
2022-08-08 08:14:07 +0300

    lesson 50. Async, defer, динамические скрипты

--- js/script.js
+++ js/script.js
@@ -1,33 +1,14 @@
-// lesson 49. События на мобильных устройствах
+// lesson 50. Async, defer, динамические скрипты
 'use strict';
-document.addEventListener('DOMContentLoaded', () => {
-
-//touchstart
-//touchmove
-//touchend
-//touchenter
-//touchleave
-//touchcancel
+// document.addEventListener('DOMContentLoaded', () => {
 
 
-    const box = document.querySelector('.box');
-
-    box.addEventListener('touchstart', (e) => {
-        e.preventDefault();
-        console.log('Start');
-        console.log(e.toches);
-    });
-    box.addEventListener('touchmove', (e) => {
-        e.preventDefault();
-        console.log('Move');
-        console.log(e.targetTouches[0].pageX);
-    });
-    box.addEventListener('touchend', (e) => {
-        e.preventDefault();
-        console.log('End');
-    });
-    //touches  свойство выдаёт список всех пальцев, которые сейчас взаимодействуют с экраном
-    //targetTouches св-во выдаёт список пальцев, которые взаимодействуют с конкретнвм элементом
-    //changedTouches -список пальцев, которые участвуют в текущем событии (например если убрать один из 4 пальцев,
-    // то выдаст этот палец в список)
-});
+const loadScript = (src)=>{
+    const script =document.createElement('script');
+    script.src = src;
+    script.async =false;
+    document.body.append(script);
+};
+loadScript("js/test.js");
+loadScript("js/test2.js");
+// });
