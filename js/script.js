0a995e749b66cad74eeba22c1808d9666b78026f
Konstantin <kuzprodev.com>
2022-08-12 14:13:17 +0300

    lesson 56. (д) Тип данных Symbol part2End

--- js/script.js
+++ js/script.js
@@ -2,23 +2,39 @@
 
 'use strict';
 // document.addEventListener('DOMContentLoaded', () => {
-let id = Symbol('id');
-const obj = {
-    name: 'Test',
-    // [Symbol('id')] : 1
-    [id]: 1,
-    getId: function(){
-        return this[id];
-    }
-};
-// let id = Symbol("id");
-// obj[id] = 1;
-// console.log(obj[id]);
-console.log(obj);
-// console.log(obj.getId());
-console.log(obj[Object.getOwnPropertySymbols(obj)[0]]);
-// console.log(obj[id]);
-for( let value in obj) console.log(value);
+// let id = Symbol('id');
+// const obj = {
+//     name: 'Test',
+//     // [Symbol('id')] : 1
+//     [id]: 1,
+//     getId: function(){
+//         return this[id];
+//     }
+// };
+// // let id = Symbol("id");
+// // obj[id] = 1;
+// // console.log(obj[id]);
+// console.log(obj);
+// // console.log(obj.getId());
+// console.log(obj[Object.getOwnPropertySymbols(obj)[0]]);
+// // console.log(obj[id]);
+// for( let value in obj) console.log(value);
 
+
+//part two
+
+const myAwesomeDB = {
+    movies: [],
+    actors: [],
+    // [Symbol('id')]: 123
+    [Symbol.for('id')]: 123
+};
+// Сторонний код библиотеки
+
+myAwesomeDB.id = '1234342414';
+
+// console.log(myAwesomeDB['id']);
+console.log(myAwesomeDB[Symbol.for('id')]);
+console.log(myAwesomeDB);
 
 // });
