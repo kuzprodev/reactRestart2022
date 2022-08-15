acdf5d4deb166d78bc6cb2ac45ad80a16ab91a8a
Konstantin <kuzprodev.com>
2022-08-15 16:42:31 +0300

    lesson 57. (д) ескрипторы свойств и полезные методы объектов  birthday

--- js/script.js
+++ js/script.js
@@ -2,14 +2,18 @@
 
 'use strict';
 // document.addEventListener('DOMContentLoaded', () => {
+const birthday = Symbol('birthday');
 const user = {
-    name: 'Alex', surname: 'Smith', birthday: '20/04/1993', showMyPublicData: function () {
+    name: 'Alex', surname: 'Smith',
+    birthday: '20/04/1993',
+    // [birthday]:'20/04/1993' ,
+    showMyPublicData: function () {
         console.log(`${this.name} ${this.surname}`);
     }
 
 };
 Object.defineProperty(user, 'birthday', {writable: false});
-Object.defineProperty(user, 'birthday', {value: prompt('Date?'), enumerable: true,configurable: true});
+// Object.defineProperty(user, 'birthday', {value: prompt('Date?'), enumerable: true,configurable: true});
 
 console.log(Object.getOwnPropertyDescriptor(user, 'birthday'));
 
@@ -23,6 +27,7 @@ console.log(Object.getOwnPropertyDescriptor(user, 'gender'));
 
 Object.defineProperty(user, 'showMyPublicData', {enumerable: false});
 for(let key in user) console.log(key);
+// console.log(Object.getOwnPropertyDescriptor(user, birthday));
 
 console.log(Object.getOwnPropertyDescriptor(Math, 'PI'));
 
