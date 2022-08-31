45fc56b9cdffdb56a489ac6196cdd39f7fc0950d
Konstantin <kuzprodev.com>
2022-08-31 08:48:24 +0300

    lesson59 map part1

--- js/script.js
+++ js/script.js
@@ -1,75 +1,82 @@
-//lesson 58. (*) Итерируемые конструкции
-
+ //lesson 59. (*) Map
+// map это специфические структуры данных, так как у них есть свои методы для работы, по типу данных это обьекты
 'use strict';
 // document.addEventListener('DOMContentLoaded', () => {
+// const user = {
+//     4: 'Alex',
+//     surname: 'Smith',
+//     birthday: '20/04/1991',
+//     showMyPublicData: function() {
+//         console.log(`${this.name} ${this.surname}`);
+//     }
+// };
+// console.log(typeof(Object.keys(user)[0])); //4 string
+const shops = [
+    {rice: 500},
+    {oil: 300},
+    {bread: 50}
+];
 
-const user = {
-    name: 'Alex', surname: 'Smith',
-    birthday: '20/04/1993',
-    showMyPublicData: function () {
-        console.log(`${this.name} ${this.surname}`);
+
+const budget = [5000, 15000, 25000];
+
+const map = new Map([
+    [{paper: 400}, 800]
+]); // массив с массивами
+
+// console.log(map.set(shops[0], 5000));
+shops.forEach((shop, i) => {
+    map.set(shop, budget[i]);
+});
+// map.set(shops[0], 5000);
+// map.set(shops[1], 15000);
+// map.set(shops[2], 54000);
+//===
+// map.set(shops[0], 5000)
+//    .set(shops[1], 15000)
+//    .set(shops[2], 54000);
+
+
+console.log(map); //внутри вся карта это массив с массивами
+
+// console.log(map.get(shops[0]));  //получить
+// console.log(map.has(shops[0])); //проверить обьект существует ли он внутри карты  true/false
+// map.delete(key);
+// map.clear();
+// map.size; //св-во- кол-во элементов на данный момент внутри карты
+
+
+// map имеет три встроенных метода для перебора map.keys()
+// map.keys(); //возвращает итерируемый обьект по ключам 
+// map.values();   //возвращает итерируемый обьект по значениям
+// map.entries() // возвращает массив с массивами(св-во и его значение)
+// map.forEach //четвертый способ как перебрать карту
+
+
+console.log(map.keys()); //те получить список всех товаров во всех магазинах
+for(let shop of map.keys()){
+    console.log(shop); 
+}
+
+const  goods =[];
+for( let shop of map.keys()){ //сначала превратили карту в массив с обьектами, потомобращаемся к каждому отдельному обьекту
+    goods.push(Object.keys(shop)[0]);//чтобы вытащить только его сво-во, тоже преобразуем в массив св-в по ключам-именно строки с товаром
+}console.log(goods);
+
+
+for(let price of map.values()){
+console.log(price);
+}
+
+for(let price of map.entries()){
+    console.log(price);
     }
-
-};
-for (const key in user) {
-    console.log(user[key]);
-}
-const arr = ['b', 'a', 'c'];
-
-for (const key in arr) {
-    console.log(arr[key]);
-}
-console.dir(arr);
-Array.prototype.someMethod = function () {
-};
-
-
-const str = 'string';
-
-for (const key in str) {
-    console.log(str[key]);
-}
-//!!!! for in перебирает свойства не по порядку!!!!!
-
-// for of проходится по значениям перебираемого обьекта
-const arr2 = ['b', 'a', 'c'];
-
-for (const key of arr2) {
-    console.log(key);
-}
-// for in получает ключ,  а for of  значения!!!
-
-const salaries = {
-    john: 500,
-    ivan: 1000,
-    anna: 5000,
-    sayHello: () => {
-        console.log('Hello');
-    }
-}
-salaries[Symbol.iterator] = function () {
-    return {
-        current: this.john,
-        last: this.anna,
-        next() {
-            if (this.current < this.last) {
-                this.current = this.current + 500;
-                return {done: false, value: this.current}
-            } else {
-                return {done: true, value: this.current}
-
-            }
-
+    for(let [shop, price] of map.entries()){ //деструтеризируем массив из двух элементов
+        console.log(price, shop);
         }
-    }
-}
 
-// for (let res of salaries) {
-//     console.log(res);
-// }
+        map.forEach((value, key, map) => {
+            console.log(key, value);
+        })
 
-//можно вызвать и вручную:
-const iterator = salaries[Symbol.iterator]();
-console.log(iterator.next()); //может понадобиться когда вызываем команду внутри обычного цикла, ведь мы можем останавливать циклы
-// при помощи break; или прерывать continue;
 // });
