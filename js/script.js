74fe20b2fb400fe3aa3b9f88f0feca7cac76155d
Konstantin <kuzprodev.com>
2022-09-02 10:00:36 +0300

    lesson 63. ClassList и делегирование событий

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 //lesson 63. ClassList и делегирование событий
 'use strict';
 // document.addEventListener('DOMContentLoaded', () => {
-const btns = document.querySelectorAll('button');
+const btns = document.querySelectorAll('button'),
+    wrapper = document.querySelector('.btn-block');
 // console.log(btns[0].classList.length);
 // console.log(btns[0].classList.item(0)); //получение класса по индексу
 // btns[0].classList.add('red', 'second', 'thr');
@@ -24,9 +25,35 @@ btns[0].addEventListener('click', () => {
 });
 
 // className не стоит использовать вообще
-console.log(btns[0].className);
+// console.log(btns[0].className);
 // part 2 -делегирование событий
 
+
+wrapper.addEventListener('click', (event) => {
+    // console.dir(event.target);
+    // if (event.target && event.target.tagName == 'BUTTON') {
+    if (event.target && event.target.matches('button.red')) {
+        console.log('любимый прием гугла');
+    };
+    if (event.target && event.target.classList.contains('blue')) {
+        console.log('click to blue btn');
+    };
+    if (event.target && event.target.classList.contains('red')) {
+        console.log('click to red btn');
+    };
+});
+
+
+// btns.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//         console.log('hello');
+//     })
+// });
+
+
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);
 
 
 
